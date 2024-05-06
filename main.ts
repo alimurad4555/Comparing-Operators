416952e9dbea50f0423c4cfe1a96ccf59af8d771
alimurad4555 <alimurad4555gmail.com>
2024-05-06 03:48:51 -0700

    comparing operators

---
+++ main.ts
@@ -0,0 +1,30 @@
+//Relation / Comparison Operators (== , === ,< , <= ,> ,>= )
+let num1 =20;
+let num2 =10;
+let num3 ='5';
+let num4 =20;
+
+// equals ==== matlab barbar english me bola to equals ya same
+console.log(num1 === num2); // --> boolean (true / false) answer is false
+
+// less then and equal <= matlb ya cheak kara ga dono ko chuta howa ya barbar too answar true daga
+console.log(num2<=num1); // --> answer true Q ka 10 < 20 chuta h
+
+
+// less then matlab chota english bola to small
+console.log(num1<num2); // --> boolean (true / false) answer is false
+console.log(num2<num1); // --> boolean answer is true
+
+// gerater then > matlab bara english bola to big 
+
+console.log(num1>num2); // --> answer is true Q ka 20 >10 bara h gerater then ha
+console.log(num2>num1); // --> answer is false Q ka 10 >20 bara nahe h gerater then nahe h 
+
+// gerater then and aquals >= matlab ka bara ya barbar english me bola to gerater then end equals 
+
+console.log(num1>=num2); // --> answer is true Q ka 20 >= 10 ae bara h gerater then h agar barbar bhe hota =equals bhe hota ture ata
+
+
+
+
+
